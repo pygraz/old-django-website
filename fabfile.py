7476466d9de0bc439b0fd4428c05c175c3d6ae28
Horst Gutmann <zerok@zerokspot.com>
2012-05-28 17:13:41 +0200

    Makes sure that we are on the right branch before starting a deployment

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,10 @@
-from fabric.api import run, cd, prefix, put, local, env, lcd
+from fabric.api import run, cd, prefix, put, local, env, lcd, abort
 
-target = 'stage'
 env_prefix = 'source %s/env/bin/activate' % (env.cwd)
 
+
 def deploy():
+    check_branch()
     prepare_package()
     upload_package()
     unpack_package()
@@ -13,6 +14,15 @@ def deploy():
     update_db()
     collect_static_files()
     start_server()
+
+
+def check_branch():
+    if not env.branch:
+        abort("No branch set for deployment")
+    branch = local("git rev-parse --abbrev-ref HEAD", capture=True)
+    if branch != env.branch:
+        abort("You are on the wrong branch for this deployment: " + branch)
+
 
 def prepare_package():
     """
@@ -31,29 +41,36 @@ def prepare_package():
 def upload_package():
     put('production.zip', 'tmp/production.zip')
 
+
 def unpack_package():
     with cd('tmp'):
         run('rm -rf app && mkdir app && cd app && unzip ../production.zip')
+
 
 def stop_server():
     with prefix(env_prefix):
         run('supervisorctl stop pygraz')
 
+
 def switch_installation():
     run('rm -rf app_previous && if [[ -d app ]]; then mv app app_previous; fi')
     run('mv tmp/app ./')
+
 
 def install_environment():
     with prefix('source env/bin/activate'):
         run('pip -E env install -r app/requirements.txt')
 
+
 def update_db():
     with prefix(env_prefix), cd('app'):
         run('python manage-production.py syncdb --noinput --migrate')
 
+
 def collect_static_files():
     with prefix(env_prefix), cd('app'):
         run('mkdir -p pygraz_website/static_collected && python manage-production.py collectstatic -c --noinput')
+
 
 def start_server():
     with prefix(env_prefix):
