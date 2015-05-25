07f862b0c4390037b2b2085eb52dcac2fbf80e0e
Horst Gutmann <zerok@zerokspot.com>
2015-05-25 15:58:54 +0200

    Cover calendar generation

--- pygraz_website/apps/meetups/tests.py
+++ pygraz_website/apps/meetups/tests.py
@@ -142,6 +142,33 @@ class MeetupViewTests(unittest.TestCase):
         self.assertEquals(self.future_meetup, resp.context['next_meetup'])
 
 
+class MeetupCalendarViewTests(TestCase):
+    def setUp(self):
+        start = arrow.utcnow().replace(weeks=+1, hour=18)
+        self.meetup = models.Meetup(start_date=start.datetime,
+                                    meetupcom_id='test')
+        self.meetup.save()
+
+    def test_simple_calendar(self):
+        resp = self.client.get('/meetups/ical/')
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual('text/calendar', resp['Content-Type'])
+        expected = """BEGIN:VCALENDAR\r
+X-WR-CALNAME:PyGRAZ-Meetups\r
+BEGIN:VEVENT\r
+SUMMARY:PyGRAZ-Meetup am {date}\r
+DTSTART;VALUE=DATE-TIME:{timestamp}\r
+UID:example.com/meetups/1\r
+DESCRIPTION:Details: https://example.com/meetups/{date}\r
+END:VEVENT\r
+END:VCALENDAR\r
+""".format(
+            date=arrow.get(self.meetup.start_date).format('YYYY-MM-DD'),
+            timestamp=arrow.get(self.meetup.start_date).format('YYYYMMDDTHHmmss') + 'Z'
+        )
+        self.assertEqual(expected, resp.content)
+
+
 class FetchRsvpsCommandTests(TestCase):
     def setUp(self):
         start = arrow.utcnow().replace(weeks=+1, hour=18)
