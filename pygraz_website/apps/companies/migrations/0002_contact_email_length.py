34934c6ebb343b61d848d138b232295a6bc66878
Horst Gutmann <zerok@zerokspot.com>
2015-07-20 18:54:02 +0200

    Some minor migration updates

---
+++ pygraz_website/apps/companies/migrations/0002_contact_email_length.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('companies', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='company',
+            name='contact_email',
+            field=models.EmailField(max_length=254, verbose_name='contact e-mail', blank=True),
+        ),
+    ]
---
+++ pygraz_website/apps/meetups/migrations/0003_field_lengths.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('meetups', '0002_rsvp_refactoring'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='rsvp',
+            name='remote_uid',
+            field=models.CharField(max_length=100, null=True, verbose_name='User ID', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='rsvp',
+            name='remote_username',
+            field=models.CharField(max_length=100, null=True, verbose_name='Username', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='session',
+            name='speaker_email',
+            field=models.EmailField(max_length=254, null=True, verbose_name='E-Mail-Adresse', blank=True),
+        ),
+    ]
