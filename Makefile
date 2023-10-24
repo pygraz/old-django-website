f2f4f9057d65f8e5b330194e1ceea342a183d25f
Thomas Aglassinger <roskakori@users.sourceforge.net>
2023-10-24 19:31:06 +0200

    #73 Restart site with Django 4

--- Makefile
+++
@@ -1,5 +0,0 @@
-all: tests
-tests:
-	export PYTHONPATH=.
-	coverage run manage-testing.py test meetups companies accounts --settings=pygraz_website.settings.testing
-	coverage report --include='pygraz_website*' --omit='*migrations*','*admin.py','*settings*'
--- pygraz_website/urls.py
+++ pygraz_website/urls.py
@@ -1,18 +1,22 @@
-from django.conf import settings
-from django.conf.urls.static import static
+"""
+URL configuration for pygraz_website project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import include, path
-
-from . import views
-
-admin.autodiscover()
+from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    # Overwrites for userena
-    path("accounts/", include("pygraz_website.apps.accounts.urls")),
-    path("accounts/", include("userena.urls")),
-    path("meetups/", include("pygraz_website.apps.meetups.urls")),
-    path("companies/", include("pygraz_website.apps.companies.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
